5a340733228e61e717eb73e6bc585adccd916ec9
jeremycryan <jeremychristopherryan@gmail.com>
2018-10-03 16:20:38 -0400

    Add slt control module

--- Lab1/slt.v
+++ Lab1/slt.v
@@ -1,12 +1,31 @@
-//  full lst
+//  Extra modules for 
 
-`define AND and #50
-`define OR or #50
-`define NOT not #50
+`define AND and #20
+`define OR or #20
+`define NOT not #10
 
-module FullSLT(
-    output out,
-    input a,
-    input b,
-    input prev
+`include "mux.v"
+
+module SLTControl(
+    input[2:0] sel,
+    output[2:0] new_sel,
+    output is_slt
 );
+
+    wire ni0, ni1, ni2;
+    wire ni0ni2;
+    wire nslt;
+
+    `NOT not1 (ni0, sel[0]);
+    `NOT not2 (ni1, sel[1]);
+    `NOT not3 (ni2, sel[2]);    // Inverted inputs
+
+    `AND and1 (ni0ni2, ni0, ni2);
+    `AND and2 (is_slt, ni0ni2, sel[1]); // Outputs true if in SLT mode.
+    `NOT not4 (nslt, is_slt);		// Outputs true if not in SLT mode.
+
+    `OR or1 (new_sel[0], is_slt, sel[0]); // If in SLT mode, set output to 001 (subtract mode)
+    `AND and3 (new_sel[1], nslt, sel[1]);
+    `AND and4 (new_sel[2], nslt, sel[2]);
+
+endmodule
