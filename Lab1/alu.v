a2c92321fdf999613fe3bae509c0d9e0bd310dd9
Nathaniel <nathaniel98.tan@gmail.com>
2018-10-03 13:42:40 -0400

    updates to alu.v

--- Lab1/alu.v
+++ Lab1/alu.v
@@ -1,11 +1,12 @@
 `include "add.v"
 `include "sub.v"
+`include "mux.v"
 
-`define AND and #50
-`define XOR xor #50
-`define OR or #50
-`define NOR nor #50
-`define NAND nand #50
+`define AND and #20
+`define XOR xor #20
+`define OR or #20
+`define NOR nor #20
+`define NAND nand #20
 
 module ALUOneBit
 (
@@ -20,4 +21,14 @@ module ALUOneBit
     wire addOut, subOut, andOut, xorOut, orOut, norOut, nandOut;
     wire addCarryOut, subCarryOut;
 
-    AdderOneBit adder(addOut,carryout,a,b,carryin);
+    AdderOneBit adder(addOut,addCarryOut,a,b,carryin);
+    SubtractorOneBit sub(subOut, subCarryOut,a,b,carryin);
+    AND andGate(andOut, a,b);
+    XOR xorGate(xorOut, a,b);
+    OR orGate(orOut, a,b);
+    NOR norGate(norOut,a,b);
+    NAND nandGate(nandOut,a,b);
+
+    mux8 (out, addOut,subOut,xorOut, 1'b0,andOut,nandOut,norOut,orOut,control);
+
+endmodule
