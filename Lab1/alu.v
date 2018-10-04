b56cce8d27ce73f1fd3ad9f5f5514c0612faafed
jeremycryan <jeremychristopherryan@gmail.com>
2018-10-03 22:17:06 -0400

    Fixed overflow on SLT

--- Lab1/alu.v
+++ Lab1/alu.v
@@ -49,12 +49,14 @@ module ALU32Bit(
     input[2:0] control
 );
     wire[31:0] tempOut;
+    wire[31:0] preOut;
     wire[2:0] newControl;
     wire is_slt;
     wire[31:0] carryouts;
     wire carryin;
     wire display_co;
     wire overflow_occur;
+    wire invert_last;
 
     SLTControl sltcontrol(control, newControl, is_slt);
 
@@ -79,10 +81,9 @@ module ALU32Bit(
     `NOR nor1 (display_co, control[1], control[2]); // Only display carryout if in ADD or SUB mode
     `AND and1 (carryout, display_co, carryouts[31]);
 
-
     // Converts non-slt outs to slt outs if is_slt is true
     // else just forwards signal
-    SLTinator sltinator(tempOut,is_slt,out);
+    SLTinator sltinator(tempOut,is_slt,preOut);
 
     nor #320 zeros (zero, out[0], out[1], out[2], out[3], out[4], out[5], out[6], out[7], out[8], out[9], out[10], out[11], out[12], out[13],
     out[14], out[15], out[16], out[17], out[18], out[19], out[20], out[21], out[22], out[23], out[24], out[25], out[26], out[27], out[28], out[29],
@@ -90,5 +91,13 @@ module ALU32Bit(
 
     `XOR overflows (overflow_occur, carryouts[30], carryouts[31]); // Calculate overflow by comparing last carryout and last carryin
     `AND show_overflow (overflow, display_co, overflow_occur);
+
+    `AND and2 (invert_last, overflow_occur, is_slt);
+    `XOR xor1 (out[0], invert_last, preOut[0]);  
+
+    integer j;
+    for (j=1;j<32;j=j+1) begin
+        assign out[j] = preOut[j];
+    end
 
 endmodule
